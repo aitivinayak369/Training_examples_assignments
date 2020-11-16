da33799bcd7e016fadb6ae6667f96230ab2f7f50
unknown <user287@ALCHEMYCLOUD.CO.IN>
2020-11-16 15:12:30 +0800

    All days trainingHtmlSpring

---
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		System.out.println("=================My new spring  app======================= ");
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
